6bacd3a5ce8928164edfe94c2e32677d81ecf850
Lorenzo Stoakes <lstoakes@gmail.com>
2014-12-16 15:53:57 +0000

    Add core decoding logic.
    This is considerably simplified vs. original approach, as now pine.js
    never outputs { field: [int], … } when it actually means { field: __id:
    [int] }, so we no longer have to check whether the struct expects an
    id/struct.

--- decoder.go
+++ decoder.go
@@ -8,6 +8,46 @@ import (
 
 	"github.com/bitly/go-simplejson"
 )
+
+// Walk the input JSON, checking for any case where the field to be written is a
+// struct or pointer to a struct, but the source is deferred as defined by
+// checkDeferred(), and set id field to unmarshal later.
+func walkJson(parent *simplejson.Json) {
+	switch getJsonNodeType(parent) {
+	case jsonObject:
+		for name, j := range getJsonFields(parent) {
+			if id, deferred := checkDeferred(j); deferred {
+				setDeferred(id, name, parent)
+			} else {
+				walkJson(j)
+			}
+		}
+	case jsonArray:
+		for _, j := range getJsonArray(parent) {
+			walkJson(j)
+		}
+	}
+	// Other fields do not need to be checked.
+}
+
+// Check whether the specified object is in fact a deferred object - if so
+// simply return the object's ID.
+func checkDeferred(node *simplejson.Json) (int, bool) {
+	if id, err := node.Get("__id").Int(); err != nil {
+		return 0, false
+	} else {
+		return id, true
+	}
+}
+
+type IdOnly struct {
+	Id int `json:"id"`
+}
+
+func setDeferred(id int, name string, parent *simplejson.Json) {
+	parent.Set(name, &IdOnly{id})
+}
+
 func unmarshal(v interface{}, j *simplejson.Json) error {
 	if b, err := j.MarshalJSON(); err != nil {
 		return err
