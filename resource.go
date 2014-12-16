ed9dabfaa6092912603c11e22a36e69eef0bd740
Lorenzo Stoakes <lstoakes@gmail.com>
2014-12-16 15:47:58 +0000

    Update resource to be more flexible
    Handles pointers e.g. []*foo, more general removing need for specific
    slice name function.

--- resource.go
+++ resource.go
@@ -2,28 +2,49 @@ package pinejs
 
 import (
 	"reflect"
-	"strconv"
 	"strings"
 
 	"github.com/fatih/structs"
 )
 
-func resourceName(res interface{}) string {
-	for _, f := range structs.Fields(res) {
+// Retrieve resource name from input struct - if contains a pinejs tag, use
+// that, otherwise use the lowercase of the struct name.
+func resourceNameFromStruct(v interface{}) string {
+	// Look for pinejs tag, use it if we find it.
+	for _, f := range structs.Fields(v) {
 		if name := f.Tag("pinejs"); name != "" {
 			return name
 		}
 	}
 
-	return strings.ToLower(structs.Name(res))
+	// Otherwise, we default to the name of the struct in lower case.
+	return strings.ToLower(structs.Name(v))
 }
 
-func resourceId(res interface{}) string {
-	id := structs.New(res).Field("Id").Value().(int)
-	return strconv.Itoa(id)
+// Unwinds pointers, slices, and slices of pointers, etc. until we get to a
+// struct then we hand off to resourceNameFromStruct.
+func resourceName(v interface{}) string {
+	ty := reflect.TypeOf(v)
+
+	switch ty.Kind() {
+	case reflect.Struct:
+		// Stay Calm and Carry on
+	case reflect.Ptr, reflect.Slice:
+		// Create new pointer to pointer/slice type.
+		ptr := reflect.New(ty.Elem())
+
+		// Deref the pointer and recurse on that value until we get to a struct.
+		el := ptr.Elem().Interface()
+		return resourceName(el)
+	default:
+		// TODO: I think this is probably impossible due to guards elsewhere.
+		logAlert.Fatalf("attempted to get resourceName of %s", ty.Kind())
+	}
+
+	return resourceNameFromStruct(v)
 }
 
-func resourceFromSlice(resSlice interface{}) interface{} {
-	resType := reflect.TypeOf(resSlice).Elem().Elem()
-	return reflect.New(resType).Interface()
+// Retrieve Id field from interface.
+func resourceId(v interface{}) int {
+	return structs.New(v).Field("Id").Value().(int)
 }
